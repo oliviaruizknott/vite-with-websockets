6d962dfc24ffe67320f6d5ccb5edf47171460e44
Olivia Ruiz-Knott <livvy@hovercraftstudio.com>
2022-11-30 15:01:39 -0700

    chore: initialize repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <p>I will test Websockets.</p>
+      <button>The button.</button>
+      <p>My ID is: _____</p>
+      <p>The button is pressed by: _____</p>
+    </div>
+  );
+};
+
+export default App;
